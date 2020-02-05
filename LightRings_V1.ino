bc9549a721d1a16b9f136f2eec03c7b3dcae3c99
Fabian <FabianBach@users.noreply.github.com>
2020-02-05 23:07:37 +0100

    Adds complementary mode and gradient effect

--- LightRings_V1.ino
+++ LightRings_V1.ino
@@ -1,13 +1,39 @@
 #include <Adafruit_NeoPixel.h>
 
-#define PIN 4
-#define STRIPSIZE 49 // Limited by max 256 bytes ram. At 3 bytes/LED you get max ~85 pixels
+enum input: byte {
+  STANDARD,
+  MODES,
+  EFFECTS
+};
+
+input selectedInput = STANDARD;
+
+enum modes: byte {
+  INDIVIDUAL,
+  COMPLEMENTARY
+};
+
+modes selectedMode = INDIVIDUAL;
+
+enum effects: byte {
+  GRADIENT,
+  PULSE
+};
+
+effects selectedEffect = GRADIENT;
+int effectIntensityPulse = 0;
+int effectIntensityGradient = 1000;
+
+#define PIN_LEDS 4
+#define NUM_LEDS 49 // Limited by max 256 bytes ram. At 3 bytes/LED you get max ~85 pixels
 
 #define PIN_ENCODER_A 0
 #define PIN_ENCODER_B 3// 1 also is the internal LED, better do not use as internal input...
 #define PIN_BUTTON 2
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(STRIPSIZE, PIN, NEO_GRB + NEO_KHZ800);
+boolean inputsDirty = false;
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN_LEDS, NEO_GRB + NEO_KHZ800);
 
 boolean LEDsActive = false;
 boolean LEDsDirty = false;
@@ -36,7 +62,7 @@ void loop()
   checkButtonInput();
   checkEncoderInput();
 
-  if (LEDsActive && LEDsDirty){
+  if (LEDsActive && inputsDirty){
     updateMainLEDs();
   }
 
@@ -48,6 +74,13 @@ void loop()
   }  
 }
 
+
+//   __    _____ ____  _____ 
+//  |  |  |   __|    \|   __|
+//  |  |__|   __|  |  |__   |
+//  |_____|_____|____/|_____|
+//  
+
 void updateStatusLED()
 {
   static unsigned long int lastUpdate = 0;
@@ -57,7 +90,7 @@ void updateStatusLED()
   if ((millis() - lastUpdate) > (1000 / 24)){ 
     lastUpdate = millis();
     
-    if (brightness >= 255){
+    if (brightness >= 254){
       direction = -1;
     }
     
@@ -66,7 +99,9 @@ void updateStatusLED()
     }
   
     brightness = (brightness + direction);
-    strip.setPixelColor(0, brightness, brightness, brightness);
+    
+    int pixelColor = strip.ColorHSV((10000 + ((selectedInput + selectedMode + selectedEffect) * 10000)) % 65535, 255, brightness);
+    strip.setPixelColor(0, pixelColor);
     LEDsDirty = true;
   }
 }
@@ -74,9 +109,17 @@ void updateStatusLED()
 void updateMainLEDs()
 {
   for (int mainUnit=0; mainUnit<2; mainUnit++){
-    for (int led=(1+(mainUnit*24)); led < (((mainUnit+1)*24)+1); led++){
-      strip.setPixelColor(led, strip.ColorHSV(mainLEDsColors[mainUnit])); // uint32_t rgbcolor = strip.ColorHSV(hue, saturation, value);
+
+    int baseColor = (selectedMode == COMPLEMENTARY) ? ((mainLEDsColors[0] + ((65535/2)*mainUnit))%65535) : mainLEDsColors[mainUnit];
+    
+    for (int led = 0; led < 24; led++){
+      int pixelGradientMultiplicator = (led < 11) ? led : (23 - led);
+      int pixelColor = (baseColor + (effectIntensityGradient * pixelGradientMultiplicator))%65535;
+
+      strip.setPixelColor(1+(mainUnit*24)+led, strip.gamma32(strip.ColorHSV(pixelColor))); 
+      // TODO: maybe use: strip.gamma32(strip.ColorHSV(hue, sat, val));
     }
+    
   }
   LEDsDirty = true;
 }
@@ -125,16 +168,53 @@ void checkButtonInput()
   if (longPushed && !stupidFlagINeed){
     stupidFlagINeed = true;
     // do something on long push
+
+    switch(selectedInput){
+      case STANDARD:
+        selectedInput = MODES;
+        break;
+
+      case MODES:
+        selectedInput = EFFECTS;
+        break;
+      
+      case EFFECTS:
+        selectedInput = STANDARD;
+        break;
+    }
+
+    inputsDirty = true;
   
   } else if (shortPushed){
-    if (!LEDsActive){
-      LEDsActive = true;
-      LEDsDirty = true;
-    } else {
-      mainLEDsSelected = (mainLEDsSelected == 0) ? 1 : 0;
+
+    switch(selectedInput){
+      case STANDARD:
+        if (!LEDsActive){
+          LEDsActive = true;
+        } else {
+          mainLEDsSelected = (mainLEDsSelected == 0) ? 1 : 0;
+        }
+        break;
+
+      case MODES:
+        selectedMode = (selectedMode == INDIVIDUAL) ? COMPLEMENTARY : INDIVIDUAL;
+        break;
+      
+      case EFFECTS:
+        selectedEffect = (selectedEffect == PULSE) ? GRADIENT : PULSE;
+        break;
     }
+
+    inputsDirty = true;
   }
 }
+
+
+//   _____ _____ _____ _____ ____  _____ _____ 
+//  |   __|   | |     |     |    \|   __| __  |
+//  |   __| | | |   --|  |  |  |  |   __|    -|
+//  |_____|_|___|_____|_____|____/|_____|__|__|
+//
  
 void checkEncoderInput()
 {
@@ -155,11 +235,37 @@ void checkEncoderInput()
   encoderPinALast = encoderPinA;
 
   if (encoderPos != 0) {
-    int newVal = mainLEDsColors[mainLEDsSelected] + (encoderPos * 200);
-    if (newVal > 65535){ newVal = newVal % 65535; }
-    if (newVal < 0){ newVal = 65535 + newVal; }
-    mainLEDsColors[mainLEDsSelected] = newVal;
-    LEDsDirty = true;
+
+    switch(selectedInput){
+      case STANDARD: {
+        int selectedLED = (selectedMode == INDIVIDUAL) ? mainLEDsSelected : 0;
+        int newVal = mainLEDsColors[selectedLED] + (encoderPos * 200);
+        if (newVal > 65535){ newVal = newVal % 65535; }
+        if (newVal < 0){ newVal = 65535 + newVal; }
+        mainLEDsColors[selectedLED] = newVal;
+        }
+        break;
+
+      case MODES:
+        // maybe different complementaries?
+        break;
+      
+      case EFFECTS:
+        switch(selectedEffect){
+          case GRADIENT:
+            effectIntensityGradient = effectIntensityGradient + (encoderPos * 50);
+            if (effectIntensityGradient < 0) {effectIntensityGradient = 0;}
+            break;
+    
+          case PULSE:
+            effectIntensityPulse = effectIntensityPulse + (encoderPos * 10);
+            if (effectIntensityPulse < 0) {effectIntensityPulse = 0;}
+            break;
+        }
+        break;
+    }
+    
+    inputsDirty = true;
   }
   
 }
